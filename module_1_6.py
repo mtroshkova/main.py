9942f6b9d0abff76c7c858320ac965642a8a2869
mtroshkova <89028062725m@gmail.com>
2024-11-28 21:25:58 +0500

    Update module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -8,3 +8,10 @@ print(my_dict)
 my_dict.pop('masha')
 print(my_dict)
 
+my_set={13,2,'perm',14,2,'svet'}
+print(my_set)
+my_set.add(86)
+my_set.add(6)
+print(my_set)
+my_set.discard(13)
+print(my_set)
